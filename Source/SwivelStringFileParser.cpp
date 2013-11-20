2778119b89cb6bb554ed1297f760dc3829e73833
pfcm <pfcmathews@gmail.com>
2013-11-20 18:31:00 +1300

    Began parser for data files

--- Source/SwivelStringFileParser.cpp
+++ Source/SwivelStringFileParser.cpp
@@ -7,3 +7,101 @@
 //
 
 #include "SwivelStringFileParser.h"
+#include <regex>
+
+using namespace std;
+
+SwivelStringFileParser::StringDataBundle* SwivelStringFileParser::parseFile(const juce::File& f)
+{
+    // first we need to turn the file into a useable stream of data
+    FileInputStream file(f);
+    // and now we can parse it
+    if (file.failedToOpen())
+    {
+        std::cerr << "Failed to open file " + f.getFileName() << std::endl;
+        return nullptr;
+    }
+    
+    StringDataBundle* data = new StringDataBundle();
+    
+    BufferedInputStream* stream = (new BufferedInputStream(&file, 256, true));
+    
+    parseSwivelStringElement(*stream, data);
+    
+    return data;
+}
+
+
+void SwivelStringFileParser::parseSwivelStringElement(juce::BufferedInputStream &file, SwivelStringFileParser::StringDataBundle *data)
+{
+    String tag = file.readNextLine();
+    if (!tag.startsWith("<swivelstring"))
+    {
+        fail("expected file to start with '<swivelstring'");
+    }
+    
+    int nindex = tag.indexOfWholeWord("number=");
+    
+    if (nindex <= 0)
+    {
+        fail("missing number attribute");
+    }
+    
+    String number = tag.substring(nindex+7); // should be the number and the end of the tag
+    number = number.trimCharactersAtEnd(">");
+    data->num = number.getIntValue();
+    
+    tag = file.readNextLine();
+    
+    while (!tag.endsWithIgnoreCase("</swivelstring>"))
+    {
+        if (file.isExhausted())
+            fail("missing end tag");
+        
+        if (tag.startsWith("<measurements"))
+            parseMeasurements(file, data, tag);
+        else if (tag.startsWith("<targets"))
+            parseTargets(file, data, tag);
+        else if (tag.startsWith("<midimsbs"))
+            parseMidiMSBS(file, data, tag);
+    }
+}
+
+void SwivelStringFileParser::parseMeasurements(juce::BufferedInputStream &file, SwivelStringFileParser::StringDataBundle *data, juce::String tag)
+{
+    int findex = tag.indexOfWholeWord("fundamental=");
+    String attr = tag.substring(findex+12);
+    attr.trimCharactersAtEnd(">");
+    data->fundamentals->add(attr.getDoubleValue());
+    
+    // now grab the list
+    String line = file.readNextLine();
+    StringArray splitLine = split(line, ",");
+    
+    Array<double>* array = new Array<double>();
+    
+    for (int i = 0; i < splitLine.size(); i++)
+        array->add(splitLine[i].getDoubleValue());
+}
+
+StringArray SwivelStringFileParser::split(juce::String s, juce::String pattern)
+{
+    StringArray array;
+    
+    split_recurse(s, pattern, array);
+    
+    return array;
+}
+
+void SwivelStringFileParser::split_recurse(juce::String s, juce::String pattern, juce::StringArray result)
+{
+    if (s.length() == 0) return;
+    
+    result.add(s.substring(0, s.indexOf(pattern)));
+    split_recurse(s.fromFirstOccurrenceOf(pattern, false, true), pattern, result);
+}
+
+void SwivelStringFileParser::fail(juce::String msg)
+{
+    throw ParseException(msg);
+}
--- Source/SwivelStringFileParser.h
+++ Source/SwivelStringFileParser.h
@@ -9,6 +9,78 @@
 #ifndef __SwivelAutotune__SwivelStringFileParser__
 #define __SwivelAutotune__SwivelStringFileParser__
 
-#include 
+#include "../JuceLibraryCode/JuceHeader.h"
+#include <regex>
+
+/*************************************************************
+ Parses data files for the strings.
+ The files are expected to follow the following grammar.
+ Line separation of the file is tremendously important,
+ see examples.
+ 
+ FILE           ::= SWIVELSTRING
+ SWIVELSTRING   ::= '<swivelstring ' S NUMATT S '>' S MEASUREMENTS+ S TARGETS S MSBS S '</swivelstring>'
+ NUMATT         ::= 'number=' NUMBER
+ MEASUREMENTS   ::= '<measurements' S FUND S '>' FLOATLIST '</measurement>'
+ TARGETS        ::= '<targets>' S FLOATLIST S '</targets>'
+ MSBS           ::= '<midimsbs>' BYTELIST '</midimsbs>'
+ FLOATLIST      ::= comma separated list of numbers with optional decimal points and fractional parts
+ BYTELIST       ::= comma separated list of numbers between 0 and 255
+ NUMBER         ::= "[0-9]"
+ S              ::= "\s"
+ 
+ *************************************************************/
+
+class SwivelStringFileParser
+{
+public:
+    /** For transporting the data found in the files */
+    struct StringDataBundle
+    {
+        /** The number of the string, used to tell them apart */
+        int num;
+        /** The measurements of the string at various frequencies */
+        Array<Array<double>*>* measured_data;
+        /** The fundamental of the string, indices corresponding to measured_data */
+        Array<double>* fundamentals;
+        /** The target frequencies */
+        Array<double>* targets;
+        /** The pitch-bend MSBs that should produce these targets */
+        Array<uint8>* midi_msbs;
+        
+        StringDataBundle()
+        {
+            measured_data = new Array<Array<double>*>();
+            fundamentals = new Array<double>();
+            targets = new Array<double>();
+            midi_msbs = new Array<uint8>();
+        }
+    };
+    
+    /** Parses the file, returns the data if it succeeds.
+     *  Should hopefully print out some meaningful errors if it doesn't (to std::cerr most likely).
+     */
+    static StringDataBundle* parseFile(const File& f);
+    
+    class ParseException : public std::exception
+    {
+    public:
+        ParseException(String message) : msg(message) {};
+        const char* what() const noexcept { return msg.getCharPointer(); };
+            
+    private:
+        String msg;
+    };
+    
+private:
+    static void parseSwivelStringElement(BufferedInputStream& file, StringDataBundle* data);
+    static void parseMeasurements(BufferedInputStream& file, StringDataBundle* data, String tag);
+    static void parseTargets(BufferedInputStream& file, StringDataBundle* data, String tag);
+    static void parseMidiMSBS(BufferedInputStream& file, StringDataBundle* data, String tag);
+    static void fail(String msg);
+    static StringArray split(String s, String pattern);
+    static void split_recurse(String s, String pattern, StringArray result);
+    
+};
 
 #endif /* defined(__SwivelAutotune__SwivelStringFileParser__) */
